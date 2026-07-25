1e83a9e0c130c5dcba02e4c39638ac31bad1ee97
Akash D P <akashdpcali@gmail.com>
2026-07-25 14:12:26 +0530

    built login page frontend

--- Chatapp-frontend/src/App.jsx
+++ Chatapp-frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css'
-import {Landing} from "./pages/Landing/Landing";
+import { Routes, Route } from "react-router-dom";
+
+import { Landing } from "./pages/Landing/Landing";
+import Login from "./pages/Login/Login";
 
 function App() {
- return(
-  <Landing />
- )
+  return (
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  )
 }
 
 export default App
---
+++ Chatapp-frontend/src/pages/Login/Login.jsx
@@ -0,0 +1,98 @@
+import { useState } from "react";
+import {
+  User,
+  Lock,
+  Eye,
+  EyeOff
+} from "lucide-react";
+
+import "./Login.css";
+
+function Login() {
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <>
+      <div className="login-app-title">Chat App</div>
+
+      <div className="login-box">
+
+        <div className="login-title">
+          LOGIN
+        </div>
+
+        <div className="mail-box">
+
+          <User className="user-avater" />
+
+          <input
+            type="email"
+            className="mail-input"
+            placeholder="Email"
+          />
+
+        </div>
+
+        <div className="password-box">
+
+          <Lock className="password-icon" />
+
+          <input
+            type={showPassword ? "text" : "password"}
+            className="password-input"
+            placeholder="Password"
+          />
+
+          {showPassword ? (
+            <EyeOff
+              className="password-toggle"
+              onClick={() => setShowPassword(false)}
+            />
+          ) : (
+            <Eye
+              className="password-toggle"
+              onClick={() => setShowPassword(true)}
+            />
+          )}
+
+        </div>
+
+        <button className="login-button">
+          LOGIN
+        </button>
+
+        <a href="" className="forgot-password">
+          Forgot Password?
+        </a>
+
+        <div className="seperation-line">
+          Or login with
+        </div>
+
+        <div className="login-with-google-div">
+
+          <img
+            src="/google-icon.svg"
+            alt="" 
+            className="google-icon"
+          />
+
+        </div>
+
+        <div className="signup-message">
+
+          Don't have an account?
+
+          <a href="" className="sighup-link">
+            Sign up
+          </a>
+
+        </div>
+
+      </div>
+    </>
+  )
+}
+
+export default Login;
